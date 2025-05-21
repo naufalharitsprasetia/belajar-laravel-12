af0a6023caeb21f698855b19b88ad5bf6b54a241
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-05-21 18:25:10 +0700

    update ui single post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -26,12 +26,17 @@ URL: https://flowbite.com/docs/components/typography/
                             <img class="mr-4 w-16 h-16 rounded-full"
                                 src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
                             <div>
-                                <a href="#" rel="author"
+                                <a href="/authors/{{ $post->author->username }}" rel="author"
                                     class="text-xl font-bold text-gray-900 dark:text-white">{{$post->author->name}}</a>
-                                <p class="text-base text-gray-500 dark:text-gray-400">{{$post->category->name}}</p>
-                                <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate
-                                        datetime="2022-02-08" title="February 8th, 2022">{{ $post->created_at }}</time>
-                                </p>
+                                <a href="/categories/{{ $post->category->slug }}"
+                                    class="block hover:underline text-gray-900">
+                                    <span
+                                        class="{{$post->category->color}} text-gray-600 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                        {{ $post->category->name }}
+                                    </span>
+                                </a>
+                                <p class="text-base text-gray-500 dark:text-gray-400">{{
+                                    $post->created_at->diffForHumans() }}</p>
                             </div>
                         </div>
                     </address>
