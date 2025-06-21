e426b85760806aa25c96eaebb4ea3d02f225a911
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-06-21 16:30:13 +0700

    integrasi dengan project sebelumnya

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout :title="$title">
+    <h1>Contact</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,31 @@
 <?php
 
+use App\Models\Post;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About', 'nama' => 'Naufal Harits']);
+});
+
+Route::get('/posts', function () {
+    // $posts = Post::with(['category', 'author'])->latest()->get();
+    $posts = Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString();
+
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+    return view('post', ['title' => ' Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
 
 Route::get('/dashboard', function () {
@@ -17,4 +38,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
